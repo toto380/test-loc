8133a40be13ee002d35cc83233b54c378afca1c4
toto380 <antoniobft.pro@gmail.com>
2026-01-30 15:55:11 +0100

    Update log.php

--- log.php
+++ log.php
@@ -1,16 +1,13 @@
 <?php
 $data = [
-    "ip" => $_SERVER['REMOTE_ADDR'],
-    "lat" => $_POST['lat'] ?? null,
-    "lon" => $_POST['lon'] ?? null,
-    "accuracy" => $_POST['acc'] ?? null,
-    "ua" => $_SERVER['HTTP_USER_AGENT'],
-    "time" => date("Y-m-d H:i:s")
+  "ip" => $_SERVER['REMOTE_ADDR'] ?? null,
+  "lat" => $_POST['lat'] ?? null,
+  "lon" => $_POST['lon'] ?? null,
+  "accuracy" => $_POST['acc'] ?? null,
+  "ua" => $_SERVER['HTTP_USER_AGENT'] ?? null,
+  "time" => date("Y-m-d H:i:s")
 ];
 
-file_put_contents(
-    "logs.txt",
-    json_encode($data) . PHP_EOL,
-    FILE_APPEND
-);
+file_put_contents("logs.txt", json_encode($data) . PHP_EOL, FILE_APPEND);
+http_response_code(204);
 ?>
